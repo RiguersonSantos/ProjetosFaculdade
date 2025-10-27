9664da69de36a432a58cd6dcf711f8b586e08af4
Riguerson <riguersonsantos899@gmail.com>
2025-10-27 20:48:56 -0300

    Adição de Arquivos Config VSCode e aula 3 de C
    Adição de 2 arquivos na pasta de projetos da faculdade

---
+++ semestre1/algoritmosElogicaEstruturada/aula3/exAula3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float produto, imposto, desconto, precoFinal; // Declara as variáveis que serão do tipo float(numeros decimais)
+    char nomeCliente[50], nomeProduto[50];
+
+
+    printf("Digite o ");
+    scanf("%f", &produto);
+}
